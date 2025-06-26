49b280783ed0a51d4fa3e90692ee897a2c4cb74f
mitchellht34 <mitchellht34@gmail.com>
2025-06-25 22:23:34 -0400

    create views

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
+from .forms import TaskForm
 
-# Create your views here.
+def index(request):
+    tasks = Task.objects.all().order_by('-due_date')
+    return render(request, 'tasks/index.html', {'tasks': tasks})
+
+def add_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/add_task.html', {'form': form})
+
+def edit_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'tasks/edit_task.html', {'form': form})
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('index')
+
+def toggle_complete(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.is_completed = not task.is_completed
+    task.save()
+    return redirect('index')
